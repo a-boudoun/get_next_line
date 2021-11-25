45d42013c8f0d3366c7f2f37ef7552a2daa3b636
abderrahim <abderrahimboudounit@gmail.com>
2021-11-25 19:15:39 +0100

    first try

---
+++ get_next_line.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aboudoun <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/24 14:43:33 by aboudoun          #+#    #+#             */
+/*   Updated: 2021/11/24 14:46:40 by aboudoun         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include<stdlib.h>
+# include<unistd.h>
+
+size_t	ft_strlen(const char *s);
+char	*ft_strdup(char *s);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+
+
+#endif
